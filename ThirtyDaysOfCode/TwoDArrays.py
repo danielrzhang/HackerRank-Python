79421964204f0e2fdf788d372b9ed0f33987f1bb
Daniel Zhang <45671923+danielrzhang@users.noreply.github.com>
2021-04-30 16:11:13 -0400

    Add files via upload

--- ThirtyDaysOfCode/TwoDArrays.py
+++ ThirtyDaysOfCode/TwoDArrays.py
@@ -3,3 +3,15 @@ if __name__ == '__main__':
 
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
+        
+    counter = 0
+    maxCounter = -2 ** 31
+    
+    for i in range(1, len(arr) - 1):
+        for j in range(1, len(arr[i]) - 1):
+            counter = arr[i - 1][j - 1] + arr[i - 1][j] + arr[i - 1][j + 1] + arr[i][j] + arr[i + 1][j - 1] + arr[i + 1][j] + arr[i + 1][j + 1];
+            
+            if (counter > maxCounter):
+                maxCounter = counter
+                
+    print(maxCounter)
